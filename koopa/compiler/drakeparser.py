6929882cecc1bcc7438d30ad1545875b05de2289
Lonnie Yu <ylonnie@cirruspath.com>
2015-07-20 19:33:49 +0000

    Created simple parser.

--- koopa/compiler/drakeparser.py
+++ koopa/compiler/drakeparser.py
@@ -19,12 +19,51 @@ class DrakeParser(object):
 
     def generate_ast(self, drake_content):
         """
-        Product an abstract-syntax-tree for the Drakefile content. 
-
+        Produce an abstract-syntax-tree for the Drakefile content. 
+        
         Keyword arguments:
         drake_content -- string content of the entire Drakefile.
-
+        
         Returns an abstract-syntax-tree. 
         """
-        print drake_content
+        
+        def add_AST_script(outputs, inputs, options, commands):
+            print 'Outputs: {}'.format(' '.join(outputs))
+            print 'Inputs: {}'.format(' '.join(inputs))
+            print 'Options: {}'.format(' '.join(options))
+            print 'Commands:\n{}'.format('\n'.join(commands))
+            return None
+        
+        lines = drake_content.split('\n')
+        seen_script = False
+        for line in lines:
+            if line != '' and line[0] != ';':
+                
+                # I/O line
+                if line[0] != ' ':
+                    if seen_script:
+                        # Add previous script to AST
+                        add_AST_script(outputs,inputs, options, commands)
+                    seen_script = True
+                    
+                    # Parse I/O line
+                    inputs = list()
+                    outputs = list()
+                    options = list()
+                    commands = list()
+                    
+                    parts = line.split('<-')
+                    outputs = parts[0].split(',')
+                    parts = parts[1].strip().split()
+                    inputs = parts[0].split(',')
+                    for token in parts[1:]:
+                        options.append(token.strip('[] '))
+                
+                # Script Command
+                else:
+                    commands.append(line)
+                
+        # Add last script to AST
+        add_AST_script(outputs,inputs, options, commands)
+        
         return None
