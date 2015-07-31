4021daadb60516c0472335bc7ed9a649b8502fc6
Lonnie Yu <ylonnie@cirruspath.com>
2015-07-31 18:54:31 +0000

    Modified example.

--- examples/luigi/count/luigi_script.py
+++ examples/luigi/count/luigi_script.py
@@ -2,11 +2,12 @@ import luigi
 from subprocess import call
 
 class InputTask0(luigi.Task):
-	def output(self): return luigi.LocalTarget('out.csv')
-	
+    def output(self): return [luigi.LocalTarget('out.csv')]
+
 class OutputTask0(luigi.Task):
-    def output(self): return luigi.LocalTarget('count.txt')
+    def requires(self): return [InputTask0()]
     def run(self): call('wc out.csv > count.txt', shell=True)
-    
+    def output(self): return [luigi.LocalTarget('count.txt')]
+
 if __name__ == '__main__':
     luigi.run()
--- examples/luigi/count/run_luigi_script.sh
+++ examples/luigi/count/run_luigi_script.sh
@@ -1,1 +1,2 @@
 python luigi_script.py OutputTask0
+
