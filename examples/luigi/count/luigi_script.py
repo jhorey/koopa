4d34b3547555f87379dcd51b4d7f2381ccc63950
Lonnie Yu <ylonnie@cirruspath.com>
2015-07-31 19:08:54 +0000

    Fixed typo.

--- examples/luigi/count/luigi_script.py
+++ examples/luigi/count/luigi_script.py
@@ -6,7 +6,7 @@ class InputTask0(luigi.Task):
 
 class OutputTask0(luigi.Task):
     def requires(self): return [InputTask0()]
-    def run(self): call('wc out.csv > count.txt', shell=True)
+    def run(self): call('  wc out.csv > count.txt ', shell=True)
     def output(self): return [luigi.LocalTarget('count.txt')]
 
 if __name__ == '__main__':
